a73916a7e78897a341f702b469e6f0702ef2fc2b
veerbhadrasolanki <veerbhadrasingh123@gmail.com>
2022-11-09 21:21:12 +0530

    Add project files.

---
+++ Metata/Pages/convert/imgtobase64.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Metata.Pages.convert
+{
+    public class imgtobase64Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Metata/wwwroot/js/convert/imgtobase64.js
@@ -0,0 +1,29 @@
+﻿const input = document.getElementById("selectAvatar");
+const avatar = document.getElementById("avatar");
+const textArea = document.getElementById("textArea");
+
+const convertBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(file);
+
+        fileReader.onload = () => {
+            resolve(fileReader.result);
+        };
+
+        fileReader.onerror = (error) => {
+            reject(error);
+        };
+    });
+};
+
+const uploadImage = async (event) => {
+    const file = event.target.files[0];
+    const base64 = await convertBase64(file);
+    avatar.src = base64;
+    textArea.innerText = base64;
+};
+
+input.addEventListener("change", (e) => {
+    uploadImage(e);
+});
